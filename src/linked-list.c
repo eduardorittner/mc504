76e6d1af54123f1e967a130162a4484fd4117481
eduardorittner <eduardorittnerc@gmail.com>
2025-03-27 21:43:59 -0300

    searcher, inserter and deleter thread functions
    We haven't decided on what to return from each one of them (only signal
    errors? Return the found/inserted/deleted node?). And code still hasn't
    been tested at all.

--- src/linked-list.c
+++ src/linked-list.c
@@ -91,3 +91,169 @@ lnode *lnode_new(size_t value) {
 }
 
 void lnode_free(lnode *node) { free(node); }
+
+/* All the operations take the same context since none of them take a position
+as argument:
+
+- Search: Searches for the first ocurrence of value
+- Insert: Inserts value at the end of the list
+- Delete: Deletes the first ocurrence of value */
+typedef struct {
+  llist *list;
+  size_t value;
+} llist_ctx;
+
+/* A searcher can only search if there is no deleter currently holding the list
+ */
+int llist_searcher_acquire(llist *list) {
+  if (pthread_mutex_lock(&list->searcher_mutex) < 0)
+    return -1;
+
+  list->searcher_count++;
+
+  /* Only the first searcher locks the no_searcher semaphore */
+  if (list->searcher_count == 1) {
+    if (sem_wait(&list->no_searcher) < 0)
+      return -1;
+  }
+
+  /* Unlock the mutex so other searchers can enter */
+  if (pthread_mutex_unlock(&list->searcher_mutex) < 0)
+    return -1;
+
+  return 0;
+}
+
+int llist_searcher_release(llist *list) {
+  if (pthread_mutex_lock(&list->searcher_mutex) < 0)
+    return -1;
+
+  list->searcher_count--;
+
+  /* Only the last searcher locks the no_searcher semaphore */
+  if (list->searcher_count == 0) {
+    if (sem_post(&list->no_searcher) < 0)
+      return -1;
+  }
+
+  if (pthread_mutex_unlock(&list->searcher_mutex) < 0)
+    return -1;
+
+  return 0;
+}
+
+/* An inserter can only enter if there are no deleters and no other inserters
+ * holding the list */
+int llist_inserter_acquire(llist *list) {
+  // Lock the inserter mutex
+  if (pthread_mutex_lock(&list->inserter) < 0)
+    return -1;
+
+  /* Since the deleter holds the no_inserter semaphore while it's active, we can
+  use it as a way to find out if there is a deleter active */
+  if (sem_wait(&list->no_inserter) < 0)
+    return -1;
+
+  return 0;
+}
+
+/* Since we're unlocking no_inserter before the inserter mutex, a waiting
+ * deleter will get priority over a waiting inserter TODO (should we do this?)
+ */
+int llist_inserter_release(llist *list) {
+  // Signal that there are currently no inserters
+  if (sem_post(&list->no_inserter) < 0)
+    return -1;
+
+  // Unlock the mutex so another inserter can lock it
+  if (pthread_mutex_unlock(&list->inserter) < 0)
+    return -1;
+
+  return 0;
+}
+
+/* A deleter can only search if there are no searchers or inserters holding
+ the list */
+int llist_deleter_acquire(llist *list) {
+  /* We need to lock the searcher_mutex to avoid a situation where a searcher
+   * locks searcher_mutex, increments search_counter but a deleter locks the
+   * no_searcher semaphore first. Thus searcher_count would incorrectly state
+   * the number of active searchers */
+  if (pthread_mutex_lock(&list->searcher_mutex) < 0)
+    return -1;
+  if (sem_wait(&list->no_searcher))
+    return -1;
+  if (sem_wait(&list->no_inserter) < 0)
+    return -1;
+
+  /* There's no need to keep the mutex locked while in the critical section
+   * since searchers will wait on no_searcher anyway */
+  if (pthread_mutex_unlock(&list->searcher_mutex) < 0)
+    return -1;
+  return 0;
+}
+
+/* Unlocks the no_searcher and no_inserter semaphores */
+int llist_deleter_release(llist *list) {
+  if (sem_post(&list->no_inserter) < 0)
+    return -1;
+  if (sem_post(&list->no_searcher) < 0)
+    return -1;
+  return 0;
+}
+
+/* TODO: What can we return?
+- Value: Redundant information since the caller already provides the value
+- Int: 0 for found, -1 not found, gives little information
+- Pointer to value: Good information, but can be invalidated by deleting it from
+the list which can risk use-after-free or double-free
+*/
+void *searcher_thread(void *args) {
+  llist_ctx ctx = *(llist_ctx *)args;
+
+  llist_searcher_acquire(ctx.list);
+
+  for (lnode *cur = ctx.list->head; cur != NULL; cur = cur->next) {
+    if (cur->value == ctx.value) {
+      int *result = malloc(sizeof(int));
+      *result = 1;
+      llist_searcher_release(ctx.list);
+      return result;
+    }
+  }
+
+  llist_searcher_release(ctx.list);
+  return 0;
+}
+
+/* TODO: What can we return?
+- Int: 0 for success, -1 on failure
+- Enum: like int but clearer intent
+- Pointer to value: Good but has the potential memory corruption problems
+*/
+void *inserter_thread(void *args) {
+  llist_ctx ctx = *(llist_ctx *)args;
+
+  llist_inserter_acquire(ctx.list);
+
+  llist_push_back(ctx.list, ctx.value);
+
+  llist_inserter_release(ctx.list);
+
+  return NULL;
+}
+
+/* TODO: What can we return?
+ */
+void *deleter_thread(void *args) {
+  llist_ctx ctx = *(llist_ctx *)args;
+
+  llist_deleter_acquire(ctx.list);
+
+  // TODO what happens when we can't delete?
+  llist_delete(ctx.list, ctx.value);
+
+  llist_deleter_release(ctx.list);
+
+  return NULL;
+}
