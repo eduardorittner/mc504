3c5884c2329d666db6d7eb432f0b99fe66055c69
lcaioporto <caio.dporto@gmail.com>
2025-04-19 17:39:09 -0300

    feat: adding new remove function and removing unnecessary get

--- src/int-list.c
+++ src/int-list.c
@@ -1,18 +1,32 @@
-#include <int-list.h>
+#include "int-list.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
 
 void int_list_init(int_list *list) {
     list->size = 0;
 }
 
-int int_list_append(int_list *list, int value) {
+int int_list_append(int_list *list, size_t value) {
     if (list->size >= MAX_INT_LIST) return 0;
     list->items[list->size++] = value;
     return 1;
 }
 
-int int_list_get(int_list *list, size_t index) {
-    if (index >= list->size) return -1;
-    return list->items[index];
+int int_list_remove(int_list *list, size_t value) {
+    size_t i;
+    for (i = 0; i < list->size; i++) {
+        if (list->items[i] == value) {
+            // Shift remaining elements to the left
+            for (size_t j = i; j < list->size - 1; j++) {
+                list->items[j] = list->items[j + 1];
+            }
+            list->size--;
+            // Successfully removed
+            return 1;
+        }
+    }
+    // Value not found
+    return 0;
 }
--- src/int-list.h
+++ src/int-list.h
@@ -1,13 +1,18 @@
+#ifndef _INT_LIST_INCLUDE_H
+#define _INT_LIST_INCLUDE_H
 #define MAX_INT_LIST 100
 
 #include <stdint.h>
 #include <stdatomic.h>
+#include <stddef.h>
 
 typedef struct {
-    int items[MAX_INT_LIST];
+    size_t items[MAX_INT_LIST];
     size_t size;
 } int_list;
 
 void int_list_init(int_list *list);
-int int_list_append(int_list *list, int value);
-int int_list_get(int_list *list, size_t index);
+int int_list_append(int_list *list, size_t value);
+int int_list_remove(int_list *list, size_t value);
+
+#endif
