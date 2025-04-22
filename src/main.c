8fde0bdedec399c6f5ed0c16ce7293dff6def591
lcaioporto <caio.dporto@gmail.com>
2025-04-22 18:03:47 -0300

    add: improving project flexibility by including the RANDOM_UPPER_BOUND constant to define the maximum value that can be choosed randomly

--- src/main.c
+++ src/main.c
@@ -4,8 +4,9 @@
 #define SEARCHERS 5
 #define INSERTERS 5
 #define DELETERS 5
+#define RANDOM_UPPER_BOUND 20
 
 int main(void) {
-  run_cfg run = run_cfg_new(INITIAL_SIZE, SEARCHERS, INSERTERS, DELETERS);
-  run_cfg_run(&run);
+  run_cfg run = run_cfg_new(INITIAL_SIZE, SEARCHERS, INSERTERS, DELETERS, RANDOM_UPPER_BOUND);
+  run_cfg_run(&run, RANDOM_UPPER_BOUND);
 }
